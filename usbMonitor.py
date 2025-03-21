c448388a018e38bef83ab7d2c631834a15eb6b8e
KhanhNguyen1308 <34126396+KhanhNguyen1308@users.noreply.github.com>
2025-03-21 11:04:46 +0700

    Create usbMonitor.py

---
+++ usbMonitor.py
@@ -0,0 +1,22 @@
+import pyudev
+import requests
+import json
+
+def monitor_usb():
+    context = pyudev.Context()
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by(subsystem='usb')
+
+    for device in iter(monitor.poll, None):
+        if device.action == 'add':
+            print("USB device connected!")
+            usb_info = {
+                'vendor_id': device.get('ID_VENDOR_ID'),
+                'product_id': device.get('ID_MODEL_ID'),
+                'serial': device.get('ID_SERIAL'),
+                'model': device.get('ID_MODEL')
+            }
+            send_usb_info_to_django(usb_info)
+
+if __name__ == "__main__":
+    monitor_usb()
